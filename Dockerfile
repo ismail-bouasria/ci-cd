9a941958361385c3dcf8d0716b933bc5d072abf9
ismail <ismail.bouasria@laplateforme.io>
2026-03-29 20:57:17 +0200

    chore: suppression fichiers redondants (docker-compose v2/v3/v4, backend Node.js, workflows obsolètes)

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-#Create MySQL Image for JSP Tutorial Application
-FROM mysql:latest
-COPY ./sqlfiles/migrate-v001.sql /docker-entrypoint-initdb.d
-EXPOSE 3306
--- server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-  console.log(`server running on port ${process.env.PORT}`);
-});
